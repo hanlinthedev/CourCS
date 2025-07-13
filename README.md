b1959628acf26fcd27e0fe21dfcdb214623aefe7
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-07-13 17:23:30 +0630

    MS CP HL

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CourCS
+# How Microsoft Copilot Helps
+
+- Help creating basic CRUD
+- Help with debugging
+- Help with writing middlewares
+- Help with providing proper error handling and responses
+- Help with writing tests
--- obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f3bd29fb20463fde37c60e5cafe740e5d29c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
